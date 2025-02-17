8f60de9f0a88ad0d9e741c311f5fb5591690f570
IslombekSadullayev <isadullayev841@gmail.com>
2025-02-17 22:44:29 +0500

    Begin10 added

---
+++ src/begin/Begin10.java
@@ -0,0 +1,13 @@
+package begin;
+
+import java.util.Scanner;
+
+public class Begin10 {
+    public static void main(String[] args) {
+        System.out.printf("Enter a= ");
+        Scanner scanner=new Scanner(System.in);
+        double a= scanner.nextDouble();
+        double yuzasi=a*a;
+        System.out.print("yuzasi= "+yuzasi);
+    }
+}
