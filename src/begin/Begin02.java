a8e6b18a4d30fb93d1888b99417f66711cba6c4c
IslombekSadullayev <isadullayev841@gmail.com>
2025-02-17 22:43:26 +0500

    Renamed

--- src/begin/Begin02.java
+++ src/begin/Begin02.java
@@ -2,7 +2,7 @@ package begin;
 
 import java.util.Scanner;
 
-public class begin2 {
+public class Begin02 {
     public static void main(String[] args) {
         System.out.printf("Enter a= ");
         Scanner scanner=new Scanner(System.in);
